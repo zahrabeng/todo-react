e55c83175e4e6288697972274350ab06ebad924f
Zahra <bghzahra@outlook.com>
2022-04-03 16:51:42 +0100

    imported data in an async function and mapped over this data for each item in the array

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react"
+import { useEffect } from "react";
+import IntTodo from "./ToDoTypes";
+
 export default function Main():JSX.Element{
+const [toDo, setToDo] = useState<IntTodo[]>([]);
+
+  useEffect(() => {
+    const getAllToDos = async () => {
+      const response = await fetch("http://localhost:4000/items");
+      const allToDos: IntTodo[] = await response.json();
+      setToDo([...allToDos]);
+    };
+    getAllToDos();
+  }, []);
+
+  const eachToDo = toDo.map(toDo => <p key={toDo.id}>{toDo.task}</p>)
+
     return(
         <>
-        {console.log("hello world")}
+        <h1>List of To Do's</h1>
+        {eachToDo}
         </>
     )
 }
