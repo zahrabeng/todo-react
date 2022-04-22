387f5e6971aedf134672e91d0fb00fe6f44a1df4
Zahra <bghzahra@outlook.com>
2022-04-22 18:40:30 +0100

    added in checkbox for done tasks use state and handler function

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,17 +6,10 @@ import axios from "axios";
 export default function Main(): JSX.Element {
   const [toDo, setToDo] = useState<IntTodo[]>([]);
   const [searchText, setSearchText] = useState<string>("");
+  const[checked , setChecked]= useState<boolean>(true);
 
   const mainListURL = "http://localhost:5000/items"
 
-  // useEffect(() => {
-  //   const getAllToDos = async () => {
-  //     const response = await fetch("http://localhost:5000/items");
-  //     const allToDos: IntTodo[] = await response.json();
-  //     setToDo([...allToDos]);
-  //   };
-  //   getAllToDos();
-  // }, [toDo]);
 
 useEffect(() => {
   async function getAllToDos(){
@@ -34,20 +27,27 @@ getAllToDos()
     setSearchText(e);
   };
 
-
-
   const handleInputClick = () =>{
     const toDoObj:IntTodo = {
       task: searchText}
     axios.post(mainListURL, toDoObj)
+    console.log()
   }
+
+
+  const handleCheck = () => {
+    setChecked(!checked);
+    console.log(checked)
+  };
 
 
   const eachToDo = toDo.map((toDo: IntTodo) => <li key={toDo.id}>{toDo.task} </li>);
 
+  //style={{ textDecorationLine: 'line-through' }}
   return (
     <>
       <h1>List of To Do's</h1>
+      
       <div>
         <input
           placeholder="Add To Do..."
@@ -55,7 +55,7 @@ getAllToDos()
           onChange={(e) => handleSearch(e.target.value)}
         ></input>
         <button onClick={handleInputClick}>Click to Add</button>
-        <input type="checkbox" placeholder="done?"></input>
+        <input type="checkbox" onClick={handleCheck}></input>
       </div>
       <ul>
         {eachToDo}
