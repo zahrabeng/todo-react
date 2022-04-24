aa0eb8c4fad6c2295252517ef0237ec3bddc5670
Zahra <bghzahra@outlook.com>
2022-04-24 03:29:16 +0100

    formatted with prettier

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,43 +6,46 @@ import axios from "axios";
 export default function Main(): JSX.Element {
   const [toDo, setToDo] = useState<IntTodo[]>([]);
   const [searchText, setSearchText] = useState<string>("");
-  const[checked , setChecked]= useState<boolean>(true);
+  const [checked, setChecked] = useState<boolean>(true);
 
-  const mainListURL = "http://localhost:5000/items"
+  const mainListURL = "http://localhost:5000/items";
 
-
-useEffect(() => {
-  async function getAllToDos(){
-  const allToDos = await axios.get(mainListURL);
-  const data:IntTodo[] = allToDos.data
-  setToDo([...data])
-}
-getAllToDos()
-}, [toDo]);
-
-
-
+  useEffect(() => {
+    async function getAllToDos() {
+      const allToDos = await axios.get(mainListURL);
+      const data: IntTodo[] = allToDos.data;
+      setToDo([...data]);
+    }
+    getAllToDos();
+  }, [toDo]);
 
   const handleSearch = (e: string) => {
     setSearchText(e);
   };
 
-  const handleInputClick = () =>{
-    const toDoObj:IntTodo = {
+  const handleInputClick = () => {
+    const toDoObj: IntTodo = {
       task: searchText,
-      done:checked
-    }
-    axios.post(mainListURL, toDoObj)
-  }
-
+      done: checked,
+    };
+    axios.post(mainListURL, toDoObj);
+  };
 
   const handleCheck = () => {
     setChecked(!checked);
   };
 
+  const deleteButton = <button>delete</button>;
 
-  const eachToDo = toDo.map((toDo: IntTodo) => {if(toDo.done === true) return <li key={toDo.id}>{toDo.task}</li> ; 
-  else if(toDo.done === false) return <li style={{ textDecorationLine: 'line-through' }} key={toDo.id}>{toDo.task}</li>});
+  const eachToDo = toDo.map((toDo: IntTodo) => {
+    if (toDo.done === true) return <li key={toDo.id}>{toDo.task}</li>;
+    else if (toDo.done === false)
+      return (
+        <li style={{ textDecorationLine: "line-through" }} key={toDo.id}>
+          {toDo.task}
+        </li>
+      );
+  });
 
   //style={{ textDecorationLine: 'line-through' }}
   return (
@@ -58,9 +61,7 @@ getAllToDos()
         <input type="checkbox" onClick={handleCheck}></input>
         <button onClick={handleInputClick}>Click to Add</button>
       </div>
-      <ul>
-        {eachToDo}
-      </ul>
+      <ul>{eachToDo}</ul>
     </>
   );
 }
